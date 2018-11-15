4de6fea0daa640609d7621030bed70afdbf33f1e
Jason Singh <Jason.Singh@baruchmail.cuny.edu>
2018-11-15 15:47:04 -0500

    jjs

--- library.js
+++ library.js
@@ -1,6 +1,8 @@
-<<<<<<< HEAD
-
-var Trainer = [];
+var Trainer1 = [];
+var Trainer2 = [];
+var Trainer3 = [];
+var Trainer4 = [];
+var Trainer5 = [];
 
 class Pokemon {
   constructor(name, hp, attack, defense, ability1, ability2) {
@@ -24,7 +26,7 @@ class Pokemon {
         console.log(data);
          let pokemon = new Pokemon(data['name'],data.stats[5].base_stat,data.stats[4].base_stat,data.stats[3].base_stat,data.abilities[0].ability.name, data.abilities[1].ability.name);
 
-
+        Trainer1.pokemon.push(4)
 
 
         }
@@ -33,29 +35,3 @@ class Pokemon {
     xhttp.open("GET", "https://pokeapi.co/api/v2/pokemon/" + id + "/", true);
     xhttp.send();
   }
-=======
-// This is the flavor text for the Pokemon
-
-function pokemonBio(pokemon) {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-      if (this.readyState == 4 && this.status == 200) {
-      data = JSON.parse(this.responseText);
-      for (i in data["flavor_text_entries"]) {
-          if (data['flavor_text_entries'][(i)]['language']['name'] == 'en'){
-          var infos = data['flavor_text_entries'][(i)]['flavor_text'];
-        }
-    let poke = new PokemonData(data.infos);
-    }
-      let node = document.createElement('p');
-        node.innerHTML = "<center><b>Bio:</b></center>" + infos;
-        document.getElementById('pokebio').appendChild(node);
-        node.style.background = "rgb(217, 217, 217, .4)";
-        node.style.borderRadius = "25px";
-        node.style.padding = "20px";
-    }
-  };
-  xhttp.open("GET", "https://pokeapi.co/api/v2/pokemon-species/" + pokemon + "/", true);
-  xhttp.send();
-}
->>>>>>> 20e79df4ad450ec4d3c4ec6ca8ae5eac25dce6a3
