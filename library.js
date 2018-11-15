29144552fb2b8ea9511d732f8d8442a64490be36
Jason Singh <Jason.Singh@baruchmail.cuny.edu>
2018-11-15 15:00:50 -0500

    main function for fetching pkemon added

---
+++ library.js
@@ -0,0 +1,34 @@
+
+var Trainer = [];
+
+class Pokemon {
+  constructor(name, hp, attack, defense, ability1, ability2) {
+    this.name= name;
+    this.hp = hp;
+    this.attack = attack;
+    this.defense = defense;
+    this.ability1 = ability1;
+    this.ability2 = ability2;
+  }
+}
+
+
+
+
+  function getPokemon(id){
+    var xhttp = new XMLHttpRequest();
+     xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+        data = JSON.parse(this.responseText)
+        console.log(data);
+         let pokemon = new Pokemon(data['name'],data.stats[5].base_stat,data.stats[4].base_stat,data.stats[3].base_stat,data.abilities[0].ability.name, data.abilities[1].ability.name);
+
+
+
+
+        }
+
+    };
+    xhttp.open("GET", "https://pokeapi.co/api/v2/pokemon/" + id + "/", true);
+    xhttp.send();
+  }
