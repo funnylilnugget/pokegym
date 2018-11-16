0f5b901387699bfe6c8900ee23c9fa9f1cf2b9c0
Bonny Mogollon <bonny.mogollon11@gmail.com>
2018-11-16 13:43:54 -0500

    added flavor_text and visible and invisible

--- main.js
+++ main.js
@@ -1,18 +1,41 @@
-var pokemon1 = 151;
-var pokemon2 = 150;
-var pokemon3 = 145;
-var pokemon4 = 345;
-var pokemon5 = 545;
-var pokemon6 = 64;
+let isDivHidden = true;
+let revealButton = document.querySelector('.btn');
+let toggleDiv = document.querySelector('.invisible');
+let toggleDiv1 = document.querySelector('.visible');
+//pokeball
+revealButton.addEventListener('click', function () {
+    if (isDivHidden === true) {
+        toggleDiv.classList.remove('invisible');
+        isDivHidden = false;
+        toggleDiv1.classList.add('invisible');
+    }
+});
+
+
+// trainer: Bonny
+var pokemon1 = 708;
+var pokemon2 = 330;
+var pokemon3 = 718;
+
+// trainer: Keri
+var pokemon4 = 144;
+var pokemon5 = 131;
+var pokemon6 = 130;
+
+// trainer: Kenny
 var pokemon7 = 381;
 var pokemon8 = 257;
 var pokemon9 = 282;
+
+// trainer: Jason
 var pokemon10= 133;
 var pokemon11 = 132;
-var pokemon12= 6;
+var pokemon12= 239;
+
+// trainer: Chris
 var pokemon13 = 356;
-var pokemon14 = 94;
-var pokemon15= 93;
+var pokemon14 = 772;
+var pokemon15=201;
 // var pokemon1 = [];
 
 
@@ -26,6 +49,7 @@ class Pokemons {
     this.images = images;
     this.type = type;
     this.abilities = abilities;
+    // this.text = text;
     trainer.pokemons.push(this);
 
   }
@@ -76,7 +100,7 @@ function getPokemon(pokemon) {
     }
     var abilities =  data["abilities"][0]["ability"]["name"];
     // console.log(abilities);
-    let pokemons = new Pokemons(name,hp,attack,defense,id,images,types,abilities);
+    let pokemons = new Pokemons(name,hp,attack,defense,id,images,types,abilities,text);
     console.log(pokemons);
     displayStats(pokemons)
 }
@@ -108,7 +132,7 @@ function pokeInf (pokemon) {
 }
 
 var trainer = new Trainer ("Bonny", pokemon1, pokemon2, pokemon3);
-var trainer2 = new Trainer("keiry", pokemon4, pokemon5, pokemon6);
+var trainer2 = new Trainer("keri", pokemon4, pokemon5, pokemon6);
 var trainer3 = new Trainer("kenny", pokemon7, pokemon8, pokemon9);
 var trainer4 = new Trainer("jason", pokemon10, pokemon11, pokemon12);
 var trainer5 = new Trainer("chris", pokemon13, pokemon14, pokemon15);
@@ -137,4 +161,29 @@ function displayStats(pokemon) {
 }
 function displayText(pokemon){
   text.innerHTML = pokemon.text;
+}
+
+// Code for flavor text
+
+function pokemonBio(pokemon) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+      data = JSON.parse(this.responseText);
+      for (i in data["flavor_text_entries"]) {
+          if (data['flavor_text_entries'][(i)]['language']['name'] == 'en'){
+          var infos = data['flavor_text_entries'][(i)]['flavor_text'];
+        }
+    let poke = new PokemonData(data.infos);
+    }
+      let node = document.createElement('p');
+        node.innerHTML = "<center><b>Bio:</b></center>" + infos;
+        document.getElementById('pokebio').appendChild(node);
+        node.style.background = "rgb(217, 217, 217, .4)";
+        node.style.borderRadius = "25px";
+        node.style.padding = "20px";
+    }
+  };
+  xhttp.open("GET", "https://pokeapi.co/api/v2/pokemon-species/" + pokemon + "/", true);
+  xhttp.send();
 }
